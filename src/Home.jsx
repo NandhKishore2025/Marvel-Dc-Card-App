2855224bcd4c916381f6cd1f5b8a885925119e81
Nandha Kishore Karthikeyan <nandhagit2025@gmail.com>
2026-03-26 21:47:19 +0530

    secound commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
                 
                />
                <button className='btn2' onClick={() => setIsDescending(!isDescending)}>
-    {isDescending ? "Sort: High to Low" : "Sort: Low to High"}
+    {isDescending ? "Sort: Low to High" : "Sort: High to Low "}
 </button>
 
                <select  className='select' value = {sortby} onChange = {e =>setSortBy(e.target.value)}>
